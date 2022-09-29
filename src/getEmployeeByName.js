d7c78cb64fbe44bd25d68f340e9a82a272e30c7c
Leandro Batista <leandrobcrv@gmail.com>
2022-09-29 18:56:31 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,21 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  if (employeeName === undefined) {
-    return {};
-  }
+  if (employeeName === undefined) { return {}; }
 
-  const primeiroNome = data.employees.filter((element) => employeeName.includes(element.firstName));
-  const ultimoNome = data.employees.filter((element) => employeeName.includes(element.lastName));
+  const primeiroNome = data.employees.find((element) => employeeName.includes(element.firstName));
+  const ultimoNome = data.employees.find((element) => employeeName.includes(element.lastName));
 
-  if (primeiroNome.length === 1) {
-    return primeiroNome[0];
-  }
-
-  if (ultimoNome.length === 1) {
-    return ultimoNome[0];
-  }
+  if (primeiroNome === undefined) { return ultimoNome; }
+  if (ultimoNome === undefined) { return primeiroNome; }
 }
 
-// console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,18 +1,11 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  const booleanGerente = data.employees.filter((element) => id.includes(element.id));
-  const contadorFuncionarios = booleanGerente[0].managers.length;
-
-  if (contadorFuncionarios > 0) {
-    return true;
-  }
-  return false;
+  // seu código aqui
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
 
-console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
-// module.exports = { isManager, getRelatedEmployees };
+module.exports = { isManager, getRelatedEmployees };
