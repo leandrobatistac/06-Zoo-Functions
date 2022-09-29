5af225c148e73173c033d34b75c4b6274156aa97
Leandro Batista <leandrobcrv@gmail.com>
2022-09-29 15:52:35 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const array = [];
 
+const getSpeciesByIds = (...idPassado) => {
+  if (idPassado.length === 0) {
+    return array;
+  }
+
+  return data.species.filter((especie) => idPassado.includes(especie.id));
+};
+
+// console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 module.exports = getSpeciesByIds;
