0b5b22b43054eb4bfd0ac17e3d5231da3924f587
Leandro Batista <leandrobcrv@gmail.com>
2022-09-29 17:23:13 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+
+  const primeiroNome = data.employees.filter((element) => employeeName.includes(element.firstName));
+  const ultimoNome = data.employees.filter((element) => employeeName.includes(element.lastName));
+
+  if (primeiroNome.length === 1) {
+    return primeiroNome[0];
+  }
+
+  if (ultimoNome.length === 1) {
+    return ultimoNome[0];
+  }
 }
 
+// console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const booleanGerente = data.employees.filter((element) => id.includes(element.id));
+  const contadorFuncionarios = booleanGerente[0].managers.length;
+
+  if (contadorFuncionarios > 0) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
 
-module.exports = { isManager, getRelatedEmployees };
+console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
+// module.exports = { isManager, getRelatedEmployees };
