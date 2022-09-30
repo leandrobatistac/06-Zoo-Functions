3f788166640d1eeadbc384cc26e167a5f27b95b8
Leandro Batista <leandrobcrv@gmail.com>
2022-09-29 23:43:49 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const resultado = {};
+
 function countAnimals(animal) {
-  // seu código aqui
+  let contador = 0;
+  if (animal === undefined) {
+    data.species.forEach((unit) => {
+      unit.residents.forEach((residente) => {
+        contador += 1;
+      });
+      resultado[unit.name] = contador;
+      contador = 0;
+    });
+    return resultado;
+  }
+  const objetoAnimalCompleto = data.species.find((element) => animal.specie.includes(element.name));
+  objetoAnimalCompleto.residents.forEach((element2) => {
+    if (animal.sex === undefined) { contador += 1; }
+    if (animal.sex === element2.sex) { contador += 1; }
+  });
+
+  return contador;
 }
 
+// console.log(countAnimals());
 module.exports = countAnimals;
