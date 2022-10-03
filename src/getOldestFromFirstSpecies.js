4ffefeab0ef989e353d2bcd3518e82ba0e1af74b
Leandro Batista <leandrobcrv@gmail.com>
2022-10-03 18:51:42 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animaisResp = data.employees.find((empregado) => empregado.id.includes(id)).responsibleFor;
+  const primeiroAnimal = data.species.find((a) => a.id.includes(animaisResp[0])).residents;
+  const maisVelho = primeiroAnimal.sort((a, b) => a.age - b.age);
+  return Object.values(maisVelho[maisVelho.length - 1]);
 }
 
 module.exports = getOldestFromFirstSpecies;
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
